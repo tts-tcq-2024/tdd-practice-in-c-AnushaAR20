80dee9553473b7b141b7344aaf8f9372c02916bf
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-11 14:37:50 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,5 +1,19 @@
 
-int add(const char* input)
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(const char* input1, const char* input2)
 {
-  return 0;
+    int num1 = atoi(input1);
+    int num2 = atoi(input2);
+    return num1 + num2;
 }
+
+int main()
+{
+  const char* str1 = "1";
+  const char* str2 = "2";
+  int result = add(str1, str2);
+  printf("result %d  \n", result);
+}
+
